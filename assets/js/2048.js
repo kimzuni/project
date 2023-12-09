0e5ab4df8d943c4b17d5f6b32a54418cff1eab95
kimzuni <me@kimzuni.com>
2023-12-09 21:52:33 +0900

    2048

--- assets/js/2048.js
+++ assets/js/2048.js
@@ -1,155 +1,179 @@
-window.addEventListener("DOMContentLoaded", function() {
-	const board = document.querySelector("#board");
+const container = document.querySelector("#container");
+const board = document.querySelector("#board");
 
-	const Board = {
-		x: 4, y: 4,
-		num: 1,
-		score: 0,
-		attr: "num",
-		state: [],
-		block: [],
-		init: function() {
-			for (let i=0; i<this.x*this.y; i++) {
-				this.state[i] = null;
+const Board = {
+	x: 4, y: 4, n_create: 1, n_mv: 0,
+	lock: true,
+	score: 0,
+	attr: "num",
+	state: [],
+	block: [],
+	play: function() {
+		if (!this.state.length) {
+			this.init();
+		} else {
+			container.classList.add("play");
+			this.lock = false;
+		}
+	},
+	pause: function() {
+		container.classList.remove("play");
+		this.lock = true;
+	},
+	init: function(x, y, n) {
+		if (this.state.length && !confirm("Restart?")) {
+			return;
+		}
+
+		if (x) this.x = x*1;
+		if (y) this.y = y*1;
+		if (n) this.n_create = n*1;
+		this.state = Array(this.x*this.y).fill(null);
+
+		board.innerHTML = "";
+		for (let y=0; y<this.y; y++) {
+			const row = document.createElement("div");
+			for (let x=0; x<this.x; x++) {
+				row.innerHTML += "<span></span>";
 			}
+			board.append(row);
+		}
 
-			board.innerHTML = "";
-			const tbody = document.createElement("tbody");
-			for (let y=0; y<this.y; y++) {
-				const tr = document.createElement("tr");
-				for (let x=0; x<this.x; x++) {
-					tr.innerHTML += "<td></td>";
-				}
-				tbody.append(tr);
+		this.n_mv = 0;
+		this.score = 0;
+		this.create(2);
+		this.update();
+		this.play();
+	},
+	nextIdxs: function(idx) {
+		if (this.x*this.y <= idx) return false;
+		const lineStart = parseInt(idx/this.x)*this.x;
+		const lineEnd = lineStart+3;
+
+		const idxs = {};
+		if (this.x <= idx)
+			idxs.up = idx - this.x;
+		if (idx < this.x*this.y-this.x)
+			idxs.down = idx + this.x;
+		if (idx !== lineStart)
+			idxs.left = idx - 1;
+		if (idx !== lineEnd)
+			idxs.right = idx + 1;
+		return idxs;
+	},
+	end: function() {
+		for (let idx=0; idx<this.state.length; idx++) {
+			const idxs = this.nextIdxs(idx);
+			for (let action in idxs) {
+				const next = idxs[action];
+				if (this.state[next] === null) return false;
+				else if (this.state[idx] === this.state[next]) return false;
 			}
-			board.append(tbody);
+		}
+		return true;
+	},
+	update: function() {
+		document.querySelector("#move-value").innerText = this.n_mv;
+		document.querySelector("#score-value").innerText = this.score;
+		for (let i=0; i<this.state.length; i++) {
+			const num = this.state[i];
+			const box = board.querySelectorAll("span")[i];
+			if (num)
+				box.setAttribute(this.attr, num);
+			else
+				box.removeAttribute(this.attr);
+		}
+		if (this.end()) {
+			alert("The End!");
+		}
+	},
+	create: function(n) {
+		const cnt = n || this.n_create;
+		let idxs = [];
+		for (let i=0; i<this.state.length; i++) {
+			if (this.state[i] === null) idxs.push(i)
+		}
+		idxs = idxs.sort(() => Math.random() - 0.5).slice(0, cnt);
 
-			this.score = 0;
-			this.create();
-			this.create();
+		for (let idx of idxs) {
+			const num = parseInt(Math.random()*100%10);
+			this.state[idx] = num != 0 ? 2 : 4;
+		}
+		this.update();
+	},
+	move: function(move) {
+		let idxs = [];
+		for (let i=0; i<this.state.length; i++) {
+			if (this.state[i] !== null) idxs.push(i)
+		}
+		if (['right', 'down'].includes(move)) idxs.reverse();
+
+		let action = false;
+		for (let idx of idxs) {
+			if (this.action(idx, idx, move)) action = true;
+		}
+		this.block = [];
+		if (action) {
+			this.n_mv += 1;
 			this.update();
-		},
-		nextIdxs: function(idx) {
-			if (this.x*this.y <= idx) return false;
-			const lineStart = parseInt(idx/this.x)*this.x;
-			const lineEnd = lineStart+3;
+			this.create();
+		}
+	},
+	action: function(orig, idx, action) {
+		const next = this.nextIdxs(idx)[action];
+		if (next === undefined) return false;
 
-			const idxs = {};
-			if (this.x <= idx)
-				idxs.up = idx - 4;
-			if (idx < this.x*this.y-this.x)
-				idxs.down = idx + 4;
-			if (idx !== lineStart)
-				idxs.left = idx - 1;
-			if (idx !== lineEnd)
-				idxs.right = idx + 1;
-			return idxs;
-		},
-		end: function() {
-			for (let idx=0; idx<this.state.length; idx++) {
-				const idxs = this.nextIdxs(idx);
-				for (let action in idxs) {
-					const next = idxs[action];
-					if (this.state[next] === null) return false;
-					else if (this.state[idx] === this.state[next]) return false;
-				}
+		if (this.state[next] !== null) {
+			if (this.block.includes(idx) || this.block.includes(next)) {
+				return false;
+			} else if (this.state[idx] === this.state[next]) {
+				const score = this.state[idx] * 2;
+				this.state[idx] *= 2;
+				this.score += this.state[idx];
+				this.block.push(idx);
+			} else {
+				return false;
 			}
-			return true;
-		},
-		update: function() {
-			document.querySelector("#score span").innerText = this.score;
-			for (let i=0; i<this.state.length; i++) {
-				const num = this.state[i];
-				const box = document.querySelectorAll("table tbody td")[i];
-				if (num)
-					box.setAttribute(this.attr, num);
-				else
-					box.removeAttribute(this.attr);
-			}
-			if (this.end()) {
-				alert("The End!");
-			}
-		},
-		create: function() {
-			const cnt = this.num;
-			let idxs = [];
-			for (let i=0; i<this.state.length; i++) {
-				if (this.state[i] === null) idxs.push(i)
-			}
-			idxs = idxs.sort(() => Math.random() - 0.5).slice(0, cnt);
+		}
 
-			for (let idx of idxs) {
-				const num = parseInt(Math.random()*100%10);
-				this.state[idx] = num != 0 ? 2 : 4;
-			}
-			this.update();
-		},
-		move: function(move) {
-			let idxs = [];
-			for (let i=0; i<this.state.length; i++) {
-				if (this.state[i] !== null) idxs.push(i)
-			}
-			if (['right', 'down'].includes(move)) idxs.reverse();
+		const indexOf = this.block.indexOf(idx);
+		if (indexOf !== -1) this.block[indexOf] = next;
+		this.state[next] = this.state[idx];
+		this.state[idx] = null;
+		this.action(orig, next, action);
+		return true;
+	},
+}
 
-			let action = false;
-			for (let idx of idxs) {
-				if (this.action(idx, idx, move)) action = true;
-			}
-			this.block = [];
-			if (action) {
-				this.update();
-				this.create();
-			}
-		},
-		action: function(orig, idx, action) {
-			const next = this.nextIdxs(idx)[action];
-			if (next === undefined) return false;
+let timeout;
+const btnClick = function(e) {
+	const btn = e.target;
+	if (btn.classList.contains('click')) clearTimeout(timeout);
+	btn.classList.add('click');
+	timeout = setTimeout(function() {
+		btn.classList.remove('click');
+	}, 50);
+	if (!Board.lock) Board.move(btn.innerText.trim().toLowerCase());
+}
 
-			if (this.state[next] !== null) {
-				if (this.block.includes(idx) || this.block.includes(next)) {
-					return false;
-				} else if (this.state[idx] === this.state[next]) {
-					const score = this.state[idx] * 2;
-					this.state[idx] *= 2;
-					this.score += this.state[idx];
-					this.block.push(idx);
-				} else {
-					return false;
-				}
-			}
+for (let btn of document.querySelectorAll("#controller button")) {
+	btn.onclick = btnClick;
+}
 
-			const indexOf = this.block.indexOf(idx);
-			if (indexOf !== -1) this.block[indexOf] = next;
-			this.state[next] = this.state[idx];
-			this.state[idx] = null;
-			this.action(orig, next, action);
-			return true;
-		},
-	}
-
-	let timeout;
-	const btnClick = function(e) {
-		const btn = e.target;
-		if (btn.classList.contains('click')) clearTimeout(timeout);
-		btn.classList.add('click');
-		timeout = setTimeout(function() {
-			btn.classList.remove('click');
-		}, 100);
-		Board.move(btn.innerText.trim().toLowerCase());
-	}
-
-	window.addEventListener("keydown", function(e) {
-		const key = e.key;
-		if (e.altKey) return;
-		else if (!key.startsWith("Arrow")) return;
-		e.preventDefault();
-		document.getElementById(key.slice(5).toLowerCase() + "Btn").click();
-	});
+document.querySelector("form").x.focus();
 
 
 
-	for (let btn of document.querySelectorAll("main button")) {
-		btn.onclick = btnClick;
-	}
-	Board.init();
+window.addEventListener("keydown", function(e) {
+	const key = e.key;
+	if (e.altKey) return;
+	else if (!key.startsWith("Arrow")) return;
+	e.preventDefault();
+	document.getElementById(key.slice(5).toLowerCase() + "Btn").click();
 });
+
+const resizeEvent = function() {
+	container.style.setProperty("--board-height", container.clientWidth + "px");
+}
+resizeEvent();
+window.addEventListener("resize", resizeEvent);
